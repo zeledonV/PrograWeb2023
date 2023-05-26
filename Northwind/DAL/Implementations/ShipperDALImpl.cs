38cf765af53b7c1ff62ee84813f143915a36b63c
zeledonV <valeria.zeledon@gmail.com>
2023-05-26 16:45:47 -0600

    trabajo en clase semana 2

---
+++ Northwind/DAL/Implementations/ShipperDALImpl.cs
@@ -0,0 +1,72 @@
+﻿using DAL.Interfaces;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Implementations
+{
+    public class ShipperDALImpl : IShipperDAL
+    {
+        private NorthwindContext _northWindContext;
+        private UnidadDeTrabajo<Shipper> unidad;
+
+        public bool Add(Shipper entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRange(IEnumerable<Shipper> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Shipper> Find(Expression<Func<Shipper, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Shipper Get(int id)
+        {
+            Shipper shipper = null;
+            using (unidad = new UnidadDeTrabajo<Shipper>(new NorthwindContext()))
+            {
+                shipper = unidad.genericDAL.Get(id);
+            }
+            return shipper;
+        }
+
+        public IEnumerable<Shipper> GetAll()
+        {
+            IEnumerable<Shipper> shippers = null;
+            using (unidad = new UnidadDeTrabajo<Shipper>(new NorthwindContext()))
+            {
+                shippers = unidad.genericDAL.GetAll();
+            }
+            return shippers;
+        }
+
+        public bool Remove(Shipper entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<Shipper> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Shipper SingleOrDefault(Expression<Func<Shipper, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Shipper entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
